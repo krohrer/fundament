1c63bfb633582d7f8b0eb24eb0042cc080b538a1
Kaspar M. Rohrer <kaspar.rohrer@gmail.com>
2014-12-29 03:41:43 +0100

    Renamed iteratee prototype modules accordingly.

--- src/profileIteratee.ml
+++ src/profileIteratee.ml
@@ -23,7 +23,8 @@ let cont_with_result =
     ignore
 
 let profile_array =
-  profile_case "Array/unfolded"
+  profile_case
+    "Array/unfolded" 
     (fun () ->
       let rec loop arr i n sum =
 	if i < n then
@@ -31,8 +32,10 @@ let profile_array =
 	else 
 	  sum
       in
-      loop int_array 0 n 0 |> cont_with_result);
-  profile_case "Array/unfolded+closure"
+      loop int_array 0 n 0 |> cont_with_result
+    );
+  profile_case
+    "Array/unfolded+closure"
     (fun () ->
       let rec loop i sum =
 	if i < n then
@@ -40,8 +43,10 @@ let profile_array =
 	else
 	  sum
       in
-      loop 0 0);
-  profile_case "Array/unfolded+closure"
+      loop 0 0
+    );
+  profile_case
+    "Array/unfolded+closure"
     (fun () ->
       let count = n in
       let arr = int_array in
@@ -51,56 +56,77 @@ let profile_array =
 	else
 	  sum
       in
-      loop 0 0);
-  profile_case "Array/sequence"
-    (fun () ->
-      let seq = Seq.from_array int_array in
-      Seq.fold (+) 0 seq |> cont_with_result);
-  profile_case "Array/fold_left" 
-    (fun () -> Array.fold_left (+) 0 int_array |> cont_with_result);
-  profile_case "Array/enum+fold"
-    (fun () -> (Enumee.array_enum int_array 0 (Enumee.fold (+)) |> cont_with_result));
-  profile_case "Array/s-enum+fold"
-    (fun () -> (SEnumee.array_enum int_array 0 (SEnumee.fold (+)) |> cont_with_result));
-  profile_case "Array/enumi+inline"
-    (fun () ->
+      loop 0 0
+    );
+  profile_case
+    "Array/sequence"
+    Seq_Prototype.(fun () ->
+      let seq = from_array int_array in
+      fold (+) 0 seq |> cont_with_result
+    );
+  profile_case
+    "Array/fold_left"
+    (fun () -> 
+      Array.fold_left (+) 0 int_array |> cont_with_result
+    );
+  profile_case
+    "Array/enum+fold"
+    Enumee_Prototype.(fun () ->
+      (array_enum int_array 0 (fold (+)) |> cont_with_result)
+    );
+  profile_case
+    "Array/s-enum+fold"
+    SEnumee_Prototype.(fun () ->
+      (array_enum int_array 0 (fold (+)) |> cont_with_result)
+    );
+  profile_case
+    "Array/enumi+inline"
+    Enumee_Prototype.(fun () ->
       let sum = ref 0 in
       let rec it a (i:int) ~cont : int=
 	sum := (+) !sum a;
 	cont i ~it
       in
-      let _ : int = Enumee.array_enumi int_array (fun i ~cont -> cont i it) in
-      !sum |> cont_with_result);
-  profile_case "Array/s-enum+inline"
-    (fun () -> (SEnumee.array_enum int_array 0 (let rec it a m b cont = 
-						  cont m ((+) a b) it
-						in
-						it) |> cont_with_result));
-  profile_case "Array/cps-enumi2+linline"
-    (fun () -> 
+      let _ : int = array_enumi int_array (fun i ~cont -> cont i it) in
+      !sum |> cont_with_result
+    );
+  profile_case
+    "Array/s-enum+inline"
+    SEnumee_Prototype.(fun () ->
+      array_enum int_array 0 (let rec it a m b cont = 
+				cont m ((+) a b) it
+			      in
+			      it) |> cont_with_result
+    );
+  profile_case
+    "Array/cps-enumi2+linline"
+    CpsEnumee_Prototype.(fun () -> 
       let rec it i cont stop a s0 s1 =
 	cont it ((+) a i) s0 s1
       in
       let k a _ _ =
 	cont_with_result a
       in
-      CpsEnumee.Array2.enumi int_array 0 k it);
-  profile_case "Array/cps_enum+inline"
-    (fun () ->
+      Array2.enumi int_array 0 k it
+    );
+  profile_case
+    "Array/cps_enum+inline"
+    CpsEnumee_Prototype.(fun () ->
       let rec it i cont stop a =
-  	cont it ((+) a i)
+	cont it ((+) a i)
       in
       let k _ a =
-  	cont_with_result a
+	cont_with_result a
       in
-      CpsEnumee.Array0.enum int_array 0 k it
+      Array0.enum int_array 0 k it
     );
-  profile_case "Array/iteratee+inline"
-    (fun () -> 
+  profile_case
+    "Array/iteratee+inline"
+    Iteratee_Prototype.(fun () -> 
       let rec it i cont a =
-	Iteratee.continue cont None it ((+) i a)
+	continue cont None it ((+) i a)
       in
-      Iteratee.Array.enum int_array (fun _ a -> a) it 0
+      Array.enum int_array (fun _ a -> a) it 0
     );
   profile_case "Array/modular-fold"
     ModularIteratee_Prototype.(fun () -> 
@@ -111,30 +137,45 @@ let profile_array =
   ()
 
 let profile_list =
-  profile_case "List/unfolded"
+  profile_case
+    "List/unfolded"
     (fun () -> 
       let rec loop sum = function
 	| [] -> sum
 	| a::rest -> loop ((+) a sum) rest
       in
-      loop 0 int_list |> cont_with_result);
-  profile_case "List/fold_left"
-    (fun () -> List.fold_left (+) 0 int_list |> cont_with_result);
-  profile_case "List/sequence"
+      loop 0 int_list |> cont_with_result
+    );
+  profile_case
+    "List/fold_left"
     (fun () ->
-      let seq = Seq.from_list int_list in
-      Seq.fold (+) 0 seq |> cont_with_result);
-  profile_case "List/enum+fold"
-    (fun () -> (Enumee.list_enum int_list 0 (Enumee.fold (+)) |> cont_with_result));
-  profile_case "List/s-enum+inline"
-    (fun () -> (SEnumee.list_enum int_list 0 (let rec it a m b cont =
-						cont m ((+) a b) it
-					      in
-					      it) |> cont_with_result));
-  profile_case "List/iteratee+inline"
-    (fun () ->
+      List.fold_left (+) 0 int_list |> cont_with_result
+    );
+  profile_case
+    "List/sequence"
+    Seq_Prototype.(fun () ->
+      let seq = from_list int_list in
+      fold (+) 0 seq |> cont_with_result
+    );
+  profile_case
+    "List/enum+fold"
+    Enumee_Prototype.(fun () ->
+      list_enum int_list 0 (fold (+)) |> cont_with_result
+    );
+  profile_case
+    "List/s-enum+inline"
+    SEnumee_Prototype.(fun () ->
+      (list_enum int_list 0 (let rec it a m b cont =
+			       cont m ((+) a b) it
+			     in
+			     it) |> cont_with_result)
+    );
+  profile_case
+    "List/iteratee+inline"
+    Iteratee_Prototype.(fun () ->
       let rec it i cont a =
-	Iteratee.continue cont None it ((+) i a)
+	continue cont None it ((+) i a)
       in
-      Iteratee.Array.enum int_array (fun _ a -> a) it 0);
+      Array.enum int_array (fun _ a -> a) it 0
+    );
   ()
